f09ae4ce4c338b34c7e90baf1ecdc25208e8b8b4
OchaMeilyla <ochameilyla@gmail.com>
2024-04-05 11:32:06 +0700

    Edited indexjs

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/static", express.static(path.join(__dirname, "static")));
 app.set("views", path.join(__dirname, "./static"));
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server started on", port);
   connection
