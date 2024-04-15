fb0fecc33f580b7421e1496c3791359a0b178f2e
OchaMeilyla <ochameilyla@gmail.com>
2024-04-15 19:24:03 +0700

    Edited vercel

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const connection = require("./app/models/database");
 const mainRouter = require("./app/routeMain");
 const app = express();
 
-app.use((req, res, next) =>{
-  res.setHeader('Access-Controll-Allow-Origin','*');
-  res.setHeader('Access-Controll-Allow-Methods' , 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Controll-Allow-Headers' , 'Content-Type, Authorization');
-  next();
-});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // http router
 app.use(mainRouter);
